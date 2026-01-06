64678669f781487fc2b0cd6b5b5564fb7769acb9
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-06 12:37:04 -0500

    Updating Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-// Strict Mode
+// Updating Objects
 
-import Message from "./components/Message";
+// It's better to group related state variables inside an object.
+// When working with objects and arrays, we need to treat them as immutable or read only.
+
+import { useState } from "react";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 }); // copying all the drink objects in the new object using the spread operator.
+  };
+
   return (
     <div>
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
