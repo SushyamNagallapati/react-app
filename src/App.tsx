3423aa302d9ec4f8e61a8cadccc770e87145166b
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-06 12:55:05 -0500

    Updating Nested Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-// Updating Objects
-
-// It's better to group related state variables inside an object.
-// When working with objects and arrays, we need to treat them as immutable or read only.
+// Updating Nested Objects
 
 import { useState } from "react";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "Chennai",
+      zipcode: 6000107,
+    },
   });
 
   const handleClick = () => {
-    setDrink({ ...drink, price: 6 }); // copying all the drink objects in the new object using the spread operator.
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipcode: 6000108 },
+    });
   };
+  // in the above object, first we are copying all the properties from the customer object,
+  // and we setting the new address object, which is completely independent of the old address object.
+  // Inside the new address object, we are calling customer address properties (by spread operator) and then we are changing the value of zipcode property.
 
   return (
     <div>
-      {drink.price}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
