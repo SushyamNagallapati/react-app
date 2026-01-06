82a70f353a0f1716490c670eed2767a20cd113f3
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-06 11:45:25 -0500

    Keeping Componets Pure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-// Choosing  the State Structure
+// Keeping Componets Pure
+// Given the same input, always returns the same result.
 
-// Best Practices
-// 1. Avoid redundant state variables.
-// 2. Group related variables inside an object.
-// 3. Avoid deeply nested structures.
+// To keep components pure, keep changes out of the render phase.
 
-import { useState } from "react";
+import Message from "./components/Message";
 
 function App() {
-  const [person, setPerson] = useState({
-    // Avoid these type of deeply nested structure, prefer flat structure
-    firstName: "",
-    lastName: "",
-    contact: {
-      address: {
-        street: "",
-      },
-    },
-  });
-  const [isLoading, setLoading] = useState(false);
-  return <div></div>;
+  return (
+    <div>
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
 }
 
 export default App;
