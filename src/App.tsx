7d6aee7c5b10d88282f414cd374b7536ad6be045
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-06 11:30:46 -0500

    Choosing the State Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-// Understanding State Hook
+// Choosing  the State Structure
 
-// 1. React updates state asynchronously.
-// 2. State is stored outside of components.
-// 3. Use hooks at the top level of component.
+// Best Practices
+// 1. Avoid redundant state variables.
+// 2. Group related variables inside an object.
+// 3. Avoid deeply nested structures.
 
 import { useState } from "react";
 
 function App() {
-  const [isVisible, setVisibility] = useState(false);
-  let count = 0;
-
-  const handleClick = () => {
-    setVisibility(true);
-    count++;
-    console.log(isVisible);
-  };
-
-  return (
-    <div>
-      <button onClick={handleClick}>Show</button>
-    </div>
-  );
+  const [person, setPerson] = useState({
+    // Avoid these type of deeply nested structure, prefer flat structure
+    firstName: "",
+    lastName: "",
+    contact: {
+      address: {
+        street: "",
+      },
+    },
+  });
+  const [isLoading, setLoading] = useState(false);
+  return <div></div>;
 }
 
 export default App;
