273a7b3a9455264bdd4dcfd48727de0e08aeaafb
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-06 11:00:35 -0500

    Understanding State Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import Like from "./components/Like";
+// Understanding State Hook
+
+// 1. React updates state asynchronously.
+// 2. State is stored outside of components.
+// 3. Use hooks at the top level of component.
+
+import { useState } from "react";
 
 function App() {
+  const [isVisible, setVisibility] = useState(false);
+  let count = 0;
+
+  const handleClick = () => {
+    setVisibility(true);
+    count++;
+    console.log(isVisible);
+  };
+
   return (
     <div>
-      <Like onClick={() => console.log("clicked!")} />
+      <button onClick={handleClick}>Show</button>
     </div>
   );
 }
