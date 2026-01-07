ad3d994ddfaa2ee2489796e5c5589ef7858a46d2
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-07 13:25:17 -0500

    Exercise - Building an ExpandableText Component (Read More/ Read Less)

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
-// Updating State
-
-// Exercise 3: Updating the array of objects
-// Eg. Adding quantities
+// Exercise - Building an ExpandableText Component (Read More/ Read Less)
 
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product1", quantity: 1 },
-      { id: 2, title: "Product2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ), // Solution
-    });
-  };
-
   return (
     <div>
-      <p>Discount: {cart.discount}</p>
-      <ul>
-        {cart.items.map((item) => (
-          <li key={item.id}>
-            {item.title} - Qty: {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleClick}>Click Me</button>
+      <ExpandableText>Hello World</ExpandableText>
     </div>
   );
 }
