e88a2abe10c355a034622046c445f71769831923
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-01-09 14:27:13 -0500

    Project - Expense Tracker (Building Expense List)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-// Creating a Form
+// Expense Tracker
 
 import { useState } from "react";
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Eggs", amount: 10, category: "Groceries" },
+    { id: 2, description: "Tomato", amount: 5, category: "Groceries" },
+    { id: 3, description: "Pasta", amount: 15, category: "Groceries" },
+    { id: 4, description: "Cream", amount: 10, category: "Groceries" },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
